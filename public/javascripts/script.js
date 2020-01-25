a493c183d03262f3f709f4aa1fc4a41bc034abfe
Thao Nhu <thaonhu2710@yahoo.com>
2020-01-25 11:27:54 +0100

    service hover fixed

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,3 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
 }, false);
 
+document.querySelector('.nav-bar-services').onmouseover = function() {
+  document.querySelector('.dropdown-content-services').classList.add('dropdown-content-hover');
+  document.querySelector('.dropdown-content-a-services').classList.add('dropdown-content-a-hover');
+};
+
+document.querySelector('.dropdown-content-services').onmouseover = function() {
+  document.querySelector('.dropdown-content-services').classList.add('dropdown-content-hover');
+  document.querySelector('.dropdown-content-a-services').classList.add('dropdown-content-a-hover');
+};
+
+document.querySelector('.dropdown-content-services').onmouseout = function() {
+  document.querySelector('.dropdown-content-services').classList.remove('dropdown-content-hover');
+  document.querySelector('.dropdown-content-a-services').classList.remove('dropdown-content-a-hover');
+};
+
+
+
+
+     
+
+       
